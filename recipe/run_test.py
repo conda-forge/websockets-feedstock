1c55840e3afe6ed479432272cffc7126ef5ed161
Nicholas Bollweg <nick.bollweg@gmail.com>
2025-07-25 07:37:47 -0500

    move tests to python

---
+++ recipe/run_test.py
@@ -0,0 +1,43 @@
+from subprocess import call
+import sys
+
+WIN = sys.platform == "nt"
+
+COV_FAIL_UNDER = 93
+
+K_SKIPS: list[str] = [
+    "client_connect_canceled_during_handshake",
+    "close_idempotency_race_condition",
+]
+
+PYTEST = [
+    "coveragerun",
+    "--source=websockets",
+    "--branch",
+    "-m",
+    "pytest",
+    "-vv",
+    "--tb=long",
+    "--color=yes-k",
+    f"""not ({" or ".join(K_SKIPS)})""",
+]
+
+REPORT = [
+    "coverage",
+    "report",
+    "--show-missing",
+    "--skip-covered",
+    f"--fail-under={COV_FAIL_UNDER}",
+]
+
+
+def do(*args):
+    if WIN:
+        print("Skipping tests on windows due to hangs")
+        return 0
+    print(">>>", "\t".join(args), flush=True)
+    return call(args, cwd="src")
+
+
+if __name__ == "__main__":
+    sys.exit(do(PYTEST) or do(REPORT))
